4ea3ad918c985b384cb07f37cc9d0812df451bd7
Luke W. Johnston <lwjohnst@gmail.com>
2025-05-12 09:09:02 +0200

    feat: :construction: added timestamp function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use chrono::{DateTime, Utc};
+use clap::{Parser, Subcommand};
+use std::time::SystemTime;
+
+/// A Rust-based implementation of the Watson CLI.
+///
+/// This is mainly a way for me to learn Rust, while also improving the
+/// [Watson](https://github.com/jazzband/Watson) time tracking CLI that
+/// is written in Python.
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Start,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Start) => {
+            println!("{}", timestamp());
+        }
+        None => {}
+    }
+}
+
+fn timestamp() -> String {
+    let now = SystemTime::now();
+    let now: DateTime<Utc> = now.into();
+    let now = now.to_rfc3339();
+
+    now
 }
